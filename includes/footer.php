30611ea21f1c7a517e8705efca85748856fbcacd
Solarfox88 <cricci.solarfox@gmail.com>
2026-05-03 16:45:23 -0700

    Add includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+    </div> <!-- container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-5cmxSjJzvAeR9Y0urxH7xPGYy4rYDF5prNHE7SUfNdoB9dXu0KYbDHlMG9ytwB3M" crossorigin="anonymous"></script>
+</body>
+</html>
