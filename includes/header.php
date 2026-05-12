4785f977b349a2e6bb359abd04c046d45048799b
Solarfox88 <cricci.solarfox@gmail.com>
2026-05-12 16:17:15 -0700

    Aggiorna menu con moduli definitivi

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestionale Condomini</title>
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-qZCRKwk9YXItg8FIU3I0GZGzGX5B9VZwxVI4UxXS5aAJlcFeRZGueMruIH2uVjZB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
@@ -18,13 +18,24 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav me-auto">
                     <?php if (is_logged_in()): ?>
                         <?php if (is_admin()): ?>
-                            <li class="nav-item"><a class="nav-link" href="/admin/dashboard.php">Dashboard Admin</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/dashboard.php">Dashboard</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/condomini.php">Condomini</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/unita.php">Unità</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/persone.php">Persone</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/documenti.php">Documenti</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/esercizi.php">Esercizi</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/movimenti.php">Movimenti</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/admin/rate.php">Rate</a></li>
                         <?php else: ?>
                             <li class="nav-item"><a class="nav-link" href="/area-condomino/dashboard.php">Area Condòmino</a></li>
                         <?php endif; ?>
+                    <?php endif; ?>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <?php if (is_logged_in()): ?>
                         <li class="nav-item"><a class="nav-link" href="/logout.php">Logout</a></li>
                     <?php else: ?>
                         <li class="nav-item"><a class="nav-link" href="/login.php">Login</a></li>
